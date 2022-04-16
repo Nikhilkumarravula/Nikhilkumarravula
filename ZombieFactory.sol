b1d99fba524e3360db4a9dc653fb88402aa77016
Nikhilkumarravula <103506842+Nikhilkumarravula@users.noreply.github.com>
2022-04-16 13:02:57 +0530

    Update ZombieFactory.sol

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity  >=0.5.0 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+
+    // declare our event here
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -15,14 +16,18 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
-    } 
+        // and fire it here
+    }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    // start here
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 
 }
 
